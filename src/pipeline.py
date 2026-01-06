03dc5dff0af8a119ba0845b406dc281fbea5ae74
Pedro Vasconcelos <pedrovasconcelos.henrique@gmail.com>
2026-01-06 17:38:11 -0300

    Adiciona dim_sellers ao banco de dados pelo pipeline (#3)

--- src/pipeline.py
+++ src/pipeline.py
@@ -10,6 +10,7 @@ STEPS = [
     {"name": "Customers (Dimensão)", "script": "src/04_dim_customers.py"},
     {"name": "Products (Dimensão)", "script": "src/05_dim_products.py"},
     {"name": "Items (Gold - Fato)", "script": "src/06_fact_items.py"},
+    {"name": "Sellers (Dimensão)", "script": "src/07_dim_sellers.py"} # <--- Verifique esta linha
 ]
 
 def run_step(step_name, script_path):
@@ -24,10 +25,10 @@ def run_step(step_name, script_path):
         subprocess.run([sys.executable, script_path], check=True)
         
         elapsed = time.time() - start_time
-        print(f"✅ SUCESSO: {step_name} finalizado em {elapsed:.2f} segundos.")
+        print(f" SUCESSO: {step_name} finalizado em {elapsed:.2f} segundos.")
         return True
     except subprocess.CalledProcessError:
-        print(f"❌ ERRO CRÍTICO: Falha em {step_name}.")
+        print(f" ERRO CRÍTICO: Falha em {step_name}.")
         return False
 
 def main():
@@ -37,7 +38,7 @@ def main():
     for step in STEPS:
         success = run_step(step["name"], step["script"])
         if not success:
-            print("\n⛔ Pipeline interrompido devido a erro.")
+            print("\n Pipeline interrompido devido a erro.")
             sys.exit(1) # Sai com código de erro
             
     total_elapsed = time.time() - total_start
